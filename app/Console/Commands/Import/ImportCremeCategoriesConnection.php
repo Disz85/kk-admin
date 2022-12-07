665b18b50c10a14fa0dd3b42c7cb138736e82976
Feher Norbert <norbert.feher@centralmediacsoport.hu>
2022-12-07 15:58:43 +0000

    Feature/km 22/creme tags import

---
+++ app/Console/Commands/Import/ImportCremeCategoriesConnection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands\Import;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ImportCremeCategoriesConnection extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:creme-categories-connection';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Imports creme categories connection from temp table file';
+
+    public function handle()
+    {
+        DB::statement("insert into categoryables (category_id, categoryable_id, categoryable_type)
+select categories.id as categoryId, products.id as productId,'App\\\Models\\\Product' from _tmp_Cremes_CremeTags
+inner join categories ON _tmp_Cremes_CremeTags.CremeTagsID = categories.legacy_id
+inner join products ON _tmp_Cremes_CremeTags.CremeID = products.legacy_id
+order by _tmp_Cremes_CremeTags.CremeID;");
+    }
+}

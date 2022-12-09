b34889d96a810555a1687312f59557b9fc32908f
Marcell Ferenc <marcell.ferenc@centralmediacsoport.hu>
2022-12-09 13:33:14 +0100

    GEN-1 Fix erroneus constant value

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::MYSQL_ATTR_LOCAL_INFILE => true,
-                PDO::MYSQL_ATTR_LOCAL_INFILE_DIRECTORY => env(MYSQL_ATTR_LOCAL_INFILE_DIRECTORY, '/tmp'),
+                PDO::MYSQL_ATTR_LOCAL_INFILE_DIRECTORY => env('MYSQL_ATTR_LOCAL_INFILE_DIRECTORY', '/tmp'),
             ]) : [],
         ],
 
